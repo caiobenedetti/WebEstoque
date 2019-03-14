beb1bfb564090e47128ae229a3596f8cea8a054b
Caio Benedetti <caio.benedetti2@gmail.com>
2019-03-13 23:01:58 -0300

    Criação dos models e controllers

---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Provider;
+use Illuminate\Http\Request;
+
+class ProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Carregar a página inicial da listagem da relação de registros que serão lidos
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //cria um registro em branco na tabela quando há um INSERT
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //passa os dados para o create
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Provider $provider)
+    {
+        //consulta do registro a ser alterado e tras para a tela
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Provider $provider)
+    {
+        //mostra o formulario de edição dos recursos
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Provider $provider)
+    {
+        //regrava os dados puxados pelo show
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Provider $provider)
+    {
+        //
+    }
+}
